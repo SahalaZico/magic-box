87d69b4ff081c49d980f3f4531a0be8fe4d11d68
SahalaZico <sahalasimamora21@gmail.com>
2019-10-22 15:39:22 +0700

    Add Circle and Rectangle Class

---
+++ Java/ShapeCollection/Circle.java
@@ -0,0 +1,57 @@
+//just my folder you can hapus "package com.shape" ya
+package com.shape;
+
+public class Circle extends Shape {
+
+    private Point p1;
+    private double diameter;
+    static final double phi = 3.14; //penggunaan static final agar nilai phi tidak bisa dirubah
+
+    public Circle() {
+        p1 = new Point(); //point berupa objek/kelas sehingga nilai defaultnya adalah constructor kosongnya
+        diameter = 0;
+
+    }
+
+    public Circle(Point newP1, double newDiameter) {
+        p1 = newP1;
+        diameter = newDiameter;
+
+        //jika anda belum mengerti konsep super anda boleh menghapusnya
+        super.setName("Circle"); //super digunakan untuk mengisi nilai attribut name pada superclass dengan setter
+    }
+
+    public Circle(Circle c) {
+        p1 = c.getP1();
+        diameter = c.getDiameter();
+
+        //jika anda belum mengerti konsep super anda boleh menghapusnya
+        super.setName(c.getName()); //super digunakan untuk mengisi nilai attribut name pada superclass dengan setter
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setP1(Point newP1) {
+        p1 = newP1;
+    }
+
+    public void setDiameter(double newDiameter) {
+        diameter = newDiameter;
+    }
+
+    @Override
+    protected double getCircumference() {
+        return phi * diameter;
+    }
+
+    public String toString() {
+        return "Circle{" + "p1=" + p1 + ", diameter=" + diameter + '}';
+    }
+
+}
---
+++ Java/ShapeCollection/Rectangle.java
@@ -0,0 +1,52 @@
+//just my folder you can hapus "package com.shape" ya
+package com.shape;
+
+//untuk dapat menggunakan fungsi mencari nilai absolut kita perlu mengimport library
+//sama halnya seperti mengimport Scanner
+import java.lang.Math;
+
+public class Rectangle extends Shape {
+
+    private Point p1;
+    private Point p2;
+
+    public Rectangle() {
+        p1 = new Point();// sama seperti penjelasan pada Circle.java
+        p2 = new Point();
+    }
+
+    public Rectangle(Point newP1, Point newP2) {
+        p1 = newP1;
+        p2 = newP2;
+    }
+
+    public Rectangle(Rectangle r1) {
+        p1 = r1.getP1();
+        p2 = r1.getP2();
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setP1(Point newP1) {
+        p1 = newP1;
+    }
+
+    public void setP2(Point newP2) {
+        p2 = newP2;
+    }
+
+    @Override
+    protected double getCircumference() {
+        //penghitungan luas dari persegi adalah panjang koordinatnya
+        //Math.abs berfungsi untuk memutlakkan nilai dari penjumlahan selisih panjang koordinatnya
+        //yang bisa saja bernilai negatif jika p2 > p1
+        return Math.abs(2 * (p1.getAbsis() - p2.getAbsis())) + Math.abs(2 * (p1.getOrdinat() - p2.getOrdinat()));
+    }
+
+}
